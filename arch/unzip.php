b4ba242c471c3391b1666bfca15dfa2df462cfd2
animov <animovoffical@gmail.com>
2021-06-11 11:51:22 +0400

    Done phpzip archivator

--- arch/unzip.php
+++ arch/unzip.php
@@ -1,1 +1,12 @@
 <?php
+
+$archive = $argv[1];
+$path = $argv[2];
+
+$data = file_get_contents($archive);
+$data = base64_decode($data);
+$data = json_decode($data);
+
+foreach ($data as $fileName => $fileData) {
+    file_put_contents($path . $fileName, $fileData);
+}
--- arch/zip.php
+++ arch/zip.php
@@ -3,14 +3,17 @@
 
 $path = $argv[1];
 $archive = $argv[2];
+$result = "";
 
 $obj = new DirectoryIterator($path);
 foreach ($obj as $file) {
         if ($file->isFile()) {
-            $file = $file->getPath() . "/" . $file->getFilename();
-            $data = file_get_contents($file);
-            $data = base64_encode($data);
-            var_dump($data);exit;
-            file_put_contents($archive, $data, FILE_APPEND);
+            $fileName = $file->getFilename();
+            $currentFile = $file->getPath() . "/" . $fileName;
+            $data[$fileName] = file_get_contents($currentFile);
+            $result  = base64_encode(json_encode($data));
+
         }
 }
+
+file_put_contents($archive, $result);
