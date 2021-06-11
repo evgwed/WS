1cad0d47217c72d9adbca4d081c19694ecaf45c8
animov <animovoffical@gmail.com>
2021-06-11 13:46:43 +0400

    Add recurse

--- arch/unzip.php
+++ arch/unzip.php
@@ -8,5 +8,14 @@ $data = base64_decode($data);
 $data = json_decode($data);
 
 foreach ($data as $fileName => $fileData) {
-    file_put_contents($path . $fileName, $fileData);
+
+    $filePath = explode("/", $fileName);
+    array_shift($filePath);
+    $file = array_pop($filePath);
+    $filePath = implode("/", $filePath);
+
+    if (!is_dir($path . $filePath)) {
+        mkdir($path . $filePath);
+    }
+    file_put_contents($path . $filePath . "/" . $file, $fileData);
 }
--- arch/zip.php
+++ arch/zip.php
@@ -4,16 +4,16 @@
 $path = $argv[1];
 $archive = $argv[2];
 $result = "";
+$files = [];
 
-$obj = new DirectoryIterator($path);
-foreach ($obj as $file) {
-        if ($file->isFile()) {
-            $fileName = $file->getFilename();
-            $currentFile = $file->getPath() . "/" . $fileName;
-            $data[$fileName] = file_get_contents($currentFile);
-            $result  = base64_encode(json_encode($data));
+$directory = new RecursiveDirectoryIterator($path);
+$iterator = new RecursiveIteratorIterator($directory);
 
-        }
+foreach ($iterator as $file) {
+            if (is_file($file)) {
+                $filePath = $file->getPathname();
+                $files[$filePath] = file_get_contents($filePath) ;
+                $result  = base64_encode(json_encode($files));
+            }
 }
-
 file_put_contents($archive, $result);
